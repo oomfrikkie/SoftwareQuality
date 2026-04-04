d05a4e385961bd417e3633d11050490a057eb324
Derjen Frick <derjenyt@gmail.com>
2026-04-04 17:56:57 +0200

    Fix XML test: use <showtitle> in test_presentation_fixed.xml and update AccessorTest to match

--- test/AccessorTest.java
+++ test/AccessorTest.java
@@ -4,25 +4,28 @@ import static org.junit.Assert.*;
 public class AccessorTest {
 
     @Test
-    public void testXMLFileHandlerStrategy() {
+    public void testXMLFileHandlerLoad() {
         try {
             Presentation p = new Presentation();
             Accessor accessor = new Accessor();
             accessor.setStrategy(new XMLFileHandler());
-            accessor.saveFile(p, "test_out.xml");
-            assertTrue(true);
+            accessor.loadFile(p, "test/resources/test_presentation_fixed.xml");
+            assertEquals("XML Test Presentation", p.getTitle());
+            assertEquals(1, p.getSize());
+            assertEquals("XML Slide 1", p.getSlide(0).getTitle());
         } catch (java.io.IOException e) {
             fail("IOException thrown: " + e.getMessage());
         }
     }
 
     @Test
-    public void testJSONFileHandlerStrategy() {
+    public void testJSONFileHandlerLoad() {
         try {
             Presentation p = new Presentation();
             Accessor accessor = new Accessor();
             accessor.setStrategy(new JSONFileHandler());
-            accessor.saveFile(p, "test_out.json");
+            accessor.loadFile(p, "test/resources/test_presentation.json");
+            // Since JSON loading is not implemented, just check that no exception is thrown
             assertTrue(true);
         } catch (java.io.IOException e) {
             fail("IOException thrown: " + e.getMessage());
